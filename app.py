2808907b6b14e6ff9aeed71532327ed1c2f88229
efrainpalencia <efrainpalencia@hotmail.com>
2025-02-07 02:22:31 -0500

    Updates dependencies and configurations for the current use case

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from server import server
 import logging
 import webbrowser
 from contextlib import redirect_stdout
@@ -5,14 +6,13 @@ from io import StringIO
 from dotenv import load_dotenv
 load_dotenv()
 
-from server import server
 
 logger = logging.getLogger(__name__)
 
 if __name__ == '__main__':
-    # open browser in a separate thread
-    with redirect_stdout(StringIO()): 
-        chrome_path = 'open -a /Applications/Google\ Chrome.app %s'           
-        webbrowser.get(chrome_path).open("http://localhost:3721")
+    # # open browser in a separate thread
+    # with redirect_stdout(StringIO()):
+    #     chrome_path = 'open -a /Applications/Google\ Chrome.app %s'
+    #     webbrowser.get(chrome_path).open("http://localhost:3721")
     # start server
-    server.run("127.0.0.1", 3721, debug=True)
+    server.run("127.0.0.1", 8000, debug=True)
--- server.py
+++ server.py
@@ -1,13 +1,14 @@
 import os
 import whisper
 from flask import Flask, render_template, request
-from chains import get_chat_chain, get_search_agent, get_qa_chain
+from chains import get_chat_chain, get_qa_chain
 import pyttsx3
 
 # get path for static files
-static_dir = os.path.join(os.path.dirname(__file__), 'static')  
-if not os.path.exists(static_dir): 
-    static_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
+static_dir = os.path.join(os.path.dirname(__file__), 'static')
+if not os.path.exists(static_dir):
+    static_dir = os.path.join(os.path.dirname(
+        os.path.abspath(__file__)), 'static')
 
 # audio file
 audio_file = 'recording.webm'
@@ -16,15 +17,17 @@ audio_file = 'recording.webm'
 model = whisper.load_model('small.en')
 
 chat_chain = get_chat_chain()
-search_agent = get_search_agent()
+# search_agent = get_search_agent()
 qa_chain = get_qa_chain()
 
 # start server
 server = Flask(__name__, static_folder=static_dir, template_folder=static_dir)
 
+
 @server.route('/')
 def landing():
     return render_template('index.html')
+
 
 @server.route('/record', methods=['POST'])
 def record():
@@ -33,7 +36,7 @@ def record():
     file.save(audio_file)
 
     # transcribe the audio file using Whisper and extract the text
-    audio = whisper.load_audio(audio_file) 
+    audio = whisper.load_audio(audio_file)
     result = model.transcribe(audio)
     text = result["text"]
 
@@ -45,7 +48,7 @@ def record():
     # output = chat_chain.predict(human_input=text)
     # output = search_agent.run(input=text)
     output = qa_chain.run(text)
-    
+
     # say out the response
     engine = pyttsx3.init()
     engine.setProperty('rate', 190)
@@ -55,6 +58,6 @@ def record():
 
     # remove the temp audio file
     if os.path.exists(audio_file):
-        os.remove(audio_file)    
-    
+        os.remove(audio_file)
+
     return {"input": text, "output": output.replace("\n", "<br />")}
